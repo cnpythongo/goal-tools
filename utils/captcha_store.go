0b51059cdf30c29798c9c9271220e5e2c7f34825
cnpythongo <cnpython@163.com>
2023-09-19 10:32:46 +0800

    fix captcha store

--- utils/captcha_store.go
+++ utils/captcha_store.go
@@ -2,37 +2,36 @@ package utils
 
 import (
 	"context"
-	"github.com/mojocn/base64Captcha"
 	"github.com/redis/go-redis/v9"
 	"time"
 )
 
-var captchaRedisExpiration = 15 * time.Minute
+var captchaRedisExpiration = 600 * time.Second // 15 minutes
 
-type captchaRedisStore struct {
+type CaptchaRedisStore struct {
 	client     *redis.Client // redis连接
 	expiration time.Duration // 有效时长
 	keyPrefix  string        // 存储键的前缀
 }
 
-func NewCaptchaRedisStore(client *redis.Client, keyPrefix string) base64Captcha.Store {
-	return &captchaRedisStore{
+func NewCaptchaRedisStore(client *redis.Client, keyPrefix string) *CaptchaRedisStore {
+	return &CaptchaRedisStore{
 		client:     client,
 		expiration: captchaRedisExpiration,
 		keyPrefix:  keyPrefix,
 	}
 }
 
-func (c *captchaRedisStore) SetExpiration(expiration time.Duration) {
+func (c *CaptchaRedisStore) SetExpiration(expiration time.Duration) {
 	c.expiration = expiration
 }
 
-func (c *captchaRedisStore) Set(id string, value string) error {
-	err := c.client.Set(context.Background(), c.keyPrefix+id, value, c.expiration*time.Second).Err()
+func (c *CaptchaRedisStore) Set(id string, value string) error {
+	err := c.client.Set(context.Background(), c.keyPrefix+id, value, c.expiration).Err()
 	return err
 }
 
-func (c *captchaRedisStore) Get(id string, clear bool) string {
+func (c *CaptchaRedisStore) Get(id string, clear bool) string {
 	ctx := context.Background()
 	var value string
 	var err error
@@ -47,6 +46,6 @@ func (c *captchaRedisStore) Get(id string, clear bool) string {
 	return value
 }
 
-func (c *captchaRedisStore) Verify(id, answer string, clear bool) (match bool) {
+func (c *CaptchaRedisStore) Verify(id, answer string, clear bool) (match bool) {
 	return c.Get(id, clear) == answer
 }
